361ed321d6f263e186c5c2c5992a49b28aee9df7
aneeltam <matleena.boral@gmail.com>
2023-10-24 18:15:01 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import Details from './pages/Details';
 
 setupIonicReact();
 
@@ -28,11 +29,10 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
+        <Route exact path="/movies" component={Home} />
+        <Route exact path="/movies/:id" component={Details} />
         <Route exact path="/">
-          <Redirect to="/home" />
+          <Redirect to="/movies" />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
